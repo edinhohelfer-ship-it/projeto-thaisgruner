2f0bd17608a03ca19963ec5d3ad9738fbc62f879
Ederson <edinhohelfer@gmail.com>
2026-07-28 19:49:30 -0300

    primeiro commit

---
+++ src/dados/sobreThais.js
@@ -0,0 +1,16 @@
+export const sobreThaisDados = {
+  kicker: "Quem é",
+  nome: "Thaís Grüner",
+  foto: "/imagens-padrao/thais-gruner.png",
+  paragrafos: [
+    "Engenheira Civil por formação, empresária há 8 anos no ramo da beleza, com mais de 7500 atendimentos, 5 anos com equipe de excelência e mais de 30K em faturamento mensal somente com procedimentos de beleza.",
+    "Expert em fidelização, atendimento encantador e atração de clientes high ticket.",
+    "Fundadora do Studio Thaís Grüner (2018), criadora do maior Workshop Técnico Lash (VRPT/RS), palestrante e mentora de empreendedoras do ramo beauty. Criadora do Método Perfect Lashes e da Mentoria Impulse Beauty (MIB).",
+  ],
+  stats: [
+    { destaque: "10 ANOS", linha1: "expertise no", linha2: "mercado beauty" },
+    { destaque: "+7500", linha1: "atendimentos", linha2: "realizados" },
+    { destaque: "+30K", linha1: "faturamento", linha2: "mensal" },
+    { destaque: "CRIADORA", linha1: "workshop", linha2: "perfect lashes" },
+  ],
+};
