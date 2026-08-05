f295762a28e66b7eefc238ae389bfec5e14d153e
Ederson <edinhohelfer@gmail.com>
2026-08-04 22:15:52 -0300

    ajuste de imagens

--- src/dados/sobreThais.js
+++ src/dados/sobreThais.js
@@ -4,8 +4,8 @@ export const sobreThaisDados = {
   foto: "/imagens-padrao/thais-gruner.png",
   paragrafos: [
     "Engenheira Civil por formação e empresária no mercado da beleza, construí minha trajetória unindo visão estratégica, excelência técnica e gestão.",
-    "Mais do que ensinar técnicas, acredito na construção de negócios sólidos.",
-    "Foi essa visão que guiou minha trajetória como empresária e que hoje compartilho por meio de mentorias, consultorias e treinamentos para profissionais da beleza que desejam crescer com direção, estrutura e consistência.",
+    "Mais do que ensinar técnicas, acredito na construção de negócios sólidos. E foi isso que me inspirou a criar o movimento M.I.B - Mulheres que Impulsionam Beleza",
+    "Foi essa visão que guiou minha trajetória como empresária e hoje compartilho essa experiência por meio da M.I.B, consultorias e treinamentos para profissionais da beleza que desejam crescer com direção, estrutura e consistência.",
   ],
   stats: [
     { destaque: "10 ANOS", linha1: "expertise no", linha2: "mercado beauty" },
